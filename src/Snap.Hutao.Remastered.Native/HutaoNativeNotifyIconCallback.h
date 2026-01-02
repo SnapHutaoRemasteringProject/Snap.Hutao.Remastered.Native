aec61a680a55aa4eea890e0c5655b9a88a1d3c4a
nahida-mouo <nahida-mouo@outlook.com>
2026-01-02 14:17:20 +0800

    修正HotKeyAction

--- src/Snap.Hutao.Remastered.Native/HutaoNativeNotifyIconCallback.h
+++ src/Snap.Hutao.Remastered.Native/HutaoNativeNotifyIconCallback.h
@@ -3,7 +3,6 @@
 #include "Types.h"
 #include <Windows.h>
 
-// 通知图标回调种类枚举
 enum class HutaoNativeNotifyIconCallbackKind : int
 {
     TaskbarCreated = 0,
@@ -12,10 +11,8 @@ enum class HutaoNativeNotifyIconCallbackKind : int
     LeftButtonDoubleClick = 3,
 };
 
-// 通知图标回调函数指针类型
 typedef void (*HutaoNativeNotifyIconCallbackFunc)(HutaoNativeNotifyIconCallbackKind kind, RECT rect, POINT point, nint userData);
 
-// 通知图标回调结构体（模仿项目风格）
 struct HutaoNativeNotifyIconCallback
 {
     HutaoNativeNotifyIconCallbackFunc value;
